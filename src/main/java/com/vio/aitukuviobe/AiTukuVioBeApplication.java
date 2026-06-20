41fb5ace189d529cedbebd07aa7c5eb6d4f12423
vioviovio <3020911615@qq.com>
2026-06-20 23:35:25 +0800

    创建后端框架springboot

---
+++ src/main/java/com/vio/aitukuviobe/AiTukuVioBeApplication.java
@@ -0,0 +1,13 @@
+package com.vio.aitukuviobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTukuVioBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiTukuVioBeApplication.class, args);
+    }
+
+}
