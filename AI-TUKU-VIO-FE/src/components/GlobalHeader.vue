6298eb2f4b3b6385118a631f7c85868e2bace71f
vioviovio <3020911615@qq.com>
2026-06-25 10:55:40 +0800

    完成用户模块：前端的开发

--- AI-TUKU-VIO-FE/src/components/GlobalHeader.vue
+++ AI-TUKU-VIO-FE/src/components/GlobalHeader.vue
@@ -22,7 +22,26 @@
       <!-- 登录组件 -->
       <div class="user-login-status">
         <div v-if="loginUserStore.loginUser.id">
-          {{ loginUserStore.loginUser.userName }}
+          <a-dropdown>
+              <a-space>
+                <a-avatar :src="loginUserStore.loginUser.userAvatar" />
+                {{ loginUserStore.loginUser.userName ?? '无名' }}
+              </a-space>
+              <template #overlay>
+                <a-menu>
+                  <a-menu-item>
+                    <router-link to="/my_space">
+                      <UserOutlined />
+                      我的空间
+                    </router-link>
+                  </a-menu-item>
+                  <a-menu-item @click="doLogout">
+                    <LogoutOutlined />
+                    退出登录
+                  </a-menu-item>
+                </a-menu>
+              </template>
+            </a-dropdown>
         </div>
         <div v-else>
           <a-button type="primary" herf="/user/login">登录</a-button>
@@ -33,15 +52,16 @@
   </div>
 </template>
 <script lang="ts" setup>
-import { h, ref } from 'vue';
-import { HomeOutlined} from '@ant-design/icons-vue';
-import type { MenuProps } from 'ant-design-vue';
+import { computed, h, ref } from 'vue';
+import { HomeOutlined,LogoutOutlined} from '@ant-design/icons-vue';
+import { message, type MenuProps } from 'ant-design-vue';
 import { useRouter } from 'vue-router';
 import { useLoginUserStore } from '@/stores/useLoginUserStore';
+import { userLogoutUsingPost } from '@/api/userController.ts';
 
 const loginUserStore = useLoginUserStore()
 
-const items = ref<MenuProps['items']>([
+const originItems = [
   {
     key: '/',
     icon: ()=>h(HomeOutlined),
@@ -49,16 +69,33 @@ const items = ref<MenuProps['items']>([
     title: '首页'
   },
   {
-    key:"/about",
-    label: '关于',
-    title: '关于'
+    key: '/admin/userManage',
+    label: '用户管理',
+    title: '用户管理',
   },
   {
     key:"others",
     label: h('a', { href: 'https://github.com/vioabio', target: '_blank' }, '作者'),
     title: '作者'
   }
-]);
+];
+
+// 根据权限过滤菜单项
+const filterMenus = (menus = [] as MenuProps['items']) => {
+  return menus?.filter((menu) => {
+    // 管理员才能看到 /admin 开头的菜单
+    if (menu?.key?.startsWith('/admin')) {
+      const loginUser = loginUserStore.loginUser
+      if (!loginUser || loginUser.userRole !== 'admin') {
+        return false
+      }
+    }
+    return true
+  })
+}
+
+// 展示在菜单的路由数组
+const items = computed(() => filterMenus(originItems))
 
 const router=useRouter();
 
@@ -76,6 +113,19 @@ router.afterEach((to) => {
   current.value = [to.path]
 })
 
+// 用户注销
+const doLogout = async () => {
+  const res = await userLogoutUsingPost()
+  if (res.data.code === 0) {
+    loginUserStore.setLoginUser({
+      userName: '未登录',
+    })
+    message.success('退出登录成功')
+    await router.push('/user/login')
+  } else {
+    message.error('退出登录失败，' + res.data.message)
+  }
+}
 
 </script>
 
